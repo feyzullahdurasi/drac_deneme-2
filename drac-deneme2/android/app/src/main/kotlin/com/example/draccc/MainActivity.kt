7ba6b35ce6e2e94621aa6c0712cc674e5637a64e
Feyzullah Durası <durasifeyzullah672@gmail.com>
2023-12-09 12:10:53 +0300

    aa

---
+++ drac-deneme2/android/app/src/main/kotlin/com/example/draccc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.draccc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- drac_deneme2/android/app/src/main/kotlin/com/example/drac_deneme2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.drac_deneme2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
